5b4a1759b1190d2fea7ba3b8c028e6d6a2dfac76
Daniil <danilabajkalov@gmail.com>
2025-02-27 19:44:54 +0300

    Add files via upload

---
+++ a.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init() #инициализация
+window_size=(300, 300) #размеры окна
+screen=pygame.display.set_mode(window_size) #создаём экран с размерами
+pygame.display.set_caption("моя игра") #название окна
+background_color = (0, 0, 225)#заливка фона цветом
+screen.fill(background_color) #применение заливки фона
+clock = pygame.time.Clock() #создание игрового таймера (фпс)
+r = pygame.Rect(150, 150, 100, 50) #создание прямоугольника
+color = (0, 0, 130)
+while True: #игровой цикл
+    clock.tick(40) #частота обновления сцены (40 кдр / с)
+    pygame.display.update() #обновление содержимого экрана
+    pygame.draw.rect(screen, color, r)
+    for event in pygame.event.get(): #проходимся по событиям
+        if event.type == pygame.QUIT: #если нажали на крестик
+            pygame.QUIT() #выход из игры
