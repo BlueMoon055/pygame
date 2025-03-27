c9589ed8f476bdecd59cb5e981e224c611ed6856
Daniil <danilabajkalov@gmail.com>
2025-03-27 20:02:10 +0300

    игра кухня

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import pygame
+
+class Food():
+    def __init__(self, a, c, d):
+        self.image = pygame.image.load(a)
+        self.rect = self.get_rect()
+        self.x = c
+        self.y = d
+
+    def draw_image(self):
+        win.blit(image, (0, 0))
+
+fon = Food()
+
+image = pygame.image.load("кухня.jpg")
+rect = image.get_rect()
 pygame.init()
-window_size=(600, 600)
+window_size=(900, 600)
 win = pygame.display.set_mode(window_size)
 color = 0, 0, 255
 x = 300
 y = 150
 rad = 100
 while True:
-    win.fill((0,0,0))  # применение заливки фона
+    win.blit(image,(0, 0))
     pygame.draw.circle(win, (0, 255, 255), (x, y), rad)
     pygame.display.update()
     for event in pygame.event.get():
